f3c963f5f3a17abf6633a8ac823e7a272575d271
alphacoderds <rahmandapr20@gmail.com>
2023-09-27 15:33:53 +0700

    Perbaikan error

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,10 +4,6 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel  : ViewModel() {
-
-    init {
-        Log.d("GameFragment", "gameViewModel created!")
-    }
     private var score = 0
     private var currentWordCount = 0
     private var _currentScrambledWord = "test"
@@ -16,23 +12,29 @@ class GameViewModel  : ViewModel() {
     private var wordsList: MutableList<String> = mutableListOf()
     private lateinit var currentWord: String
 
+    init {
+        Log.d("GameFragment", "gameViewModel created!")
+    }
+
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
     }
+
     private fun getNextWord() {
         currentWord = allWordsList.random()
         val tempWord = currentWord.toCharArray()
-        tempWord.shuffle(
-    }
-    while (String(tempWord).equals(currentWord, false)) {
         tempWord.shuffle()
-    }
-    if (wordsList.contains(currentWord)) {
-        getNextWord()
-    } else {
-        _currentScrambledWord = String(tempWord)
-        ++currentWordCount
-        wordsList.add(currentWord)
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
     }
 }
