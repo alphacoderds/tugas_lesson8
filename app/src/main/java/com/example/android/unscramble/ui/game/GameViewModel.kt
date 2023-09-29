755fb733221188e143d9341b8e4bd5b2b5ea595f
alphacoderds <rahmandapr20@gmail.com>
2023-09-29 21:31:45 +0700

    	Di GameViewModel mengubah variabel currentScrubledWord

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.android.unscramble.ui.game
 
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.TtsSpan
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import java.lang.reflect.Array.get
 
 class GameViewModel : ViewModel() {
     private val _score = MutableLiveData(0)
@@ -15,10 +20,21 @@ class GameViewModel : ViewModel() {
         get() = _currentWordCount
 
     private val _currentScrambledWord = MutableLiveData<String>()
-    val currentScrambledWord: LiveData<String>
-        get() = _currentScrambledWord
-    private var wordsList: MutableList<String> = mutableListOf()
-    private lateinit var currentWord: String
+    val currentScrambledWord: LiveData<Spannable> = Transformations.map(_currentScrambledWord) {
+        if (it == null) {
+            SpannableString("")
+        } else {
+            val scrambledWord = it.toString()
+            val spannable: Spannable = SpannableString(scrambledWord)
+            spannable.setSpan(
+                TtsSpan.VerbatimBuilder(scrambledWord).build(),
+                0,
+                scrambledWord.length,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            spannable
+        }
+    }
 
     init {
         Log.d("GameFragment", "gameViewModel created!")
@@ -34,7 +50,6 @@ class GameViewModel : ViewModel() {
         currentWord = allWordsList.random()
         val tempWord = currentWord.toCharArray()
         tempWord.shuffle()
-
         while (String(tempWord).equals(currentWord, false)) {
             tempWord.shuffle()
         }
@@ -46,6 +61,9 @@ class GameViewModel : ViewModel() {
             wordsList.add(currentWord)
         }
     }
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
 
     fun reinitializeData() {
         _score.value = 0
@@ -66,6 +84,7 @@ class GameViewModel : ViewModel() {
         return false
     }
 
+
     fun nextWord(): Boolean {
         return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
             getNextWord()
@@ -73,4 +92,6 @@ class GameViewModel : ViewModel() {
         } else false
     }
 }
+
+
 
